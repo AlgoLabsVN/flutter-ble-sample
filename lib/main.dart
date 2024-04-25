391de41f8c6856d5ea1116da5ed8514b496bdcb5
Đặng Hải Long <longdh@algolabs.vn>
2024-04-25 12:05:15 +0700

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:location_permissions/location_permissions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Some state management stuff
+  bool _foundDeviceWaitingToConnect = false;
+  bool _scanStarted = false;
+  bool _connected = false;
+// Bluetooth related variables
+  late DiscoveredDevice _ubiqueDevice;
+  final flutterReactiveBle = FlutterReactiveBle();
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late QualifiedCharacteristic _rxCharacteristic;
+// These are the UUIDs of your device
+  final Uuid serviceUuid = Uuid.parse("75C276C3-8F97-20BC-A143-B354244886D4");
+  final Uuid characteristicUuid =
+      Uuid.parse("6ACF4F08-CC9D-D495-6B41-AA7E60C4E8A6");
+
+  void _startScan() async {
+// Platform permissions handling stuff
+    bool permGranted = false;
+    setState(() {
+      _scanStarted = true;
+    });
+    PermissionStatus permission;
+    if (Platform.isAndroid) {
+      permission = await LocationPermissions().requestPermissions();
+      if (permission == PermissionStatus.granted) permGranted = true;
+    } else if (Platform.isIOS) {
+      permGranted = true;
+    }
+
+// Main scanning logic happens here ⤵️
+    if (permGranted) {
+      _scanStream =
+          flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
+        if (device.name.contains("IND236")) {
+          print(device.name);
+        }
+      });
+    }
+  }
+
+  void _connectToDevice() {
+    // We're done scanning, we can cancel it
+    _scanStream.cancel();
+    // Let's listen to our connection so we can make updates on a state change
+    Stream<ConnectionStateUpdate> _currentConnectionStream = flutterReactiveBle
+        .connectToAdvertisingDevice(
+            id: _ubiqueDevice.id,
+            prescanDuration: const Duration(seconds: 1),
+            withServices: [serviceUuid, characteristicUuid]);
+    _currentConnectionStream.listen((event) {
+      switch (event.connectionState) {
+        // We're connected and good to go!
+        case DeviceConnectionState.connected:
+          {
+            _rxCharacteristic = QualifiedCharacteristic(
+                serviceId: serviceUuid,
+                characteristicId: characteristicUuid,
+                deviceId: event.deviceId);
+            setState(() {
+              _foundDeviceWaitingToConnect = false;
+              _connected = true;
+            });
+            break;
+          }
+        // Can add various state state updates on disconnect
+        case DeviceConnectionState.disconnected:
+          {
+            break;
+          }
+        default:
+      }
+    });
+  }
+
+  void _partyTime() {
+    if (_connected) {
+      flutterReactiveBle
+          .writeCharacteristicWithResponse(_rxCharacteristic, value: [
+        0xff,
+      ]);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+      persistentFooterButtons: [
+        ElevatedButton(
+          onPressed: _startScan,
+          child: const Icon(Icons.search),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Icon(Icons.bluetooth),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Icon(Icons.celebration_rounded),
+        ),
+      ],
+    );
+  }
+}
